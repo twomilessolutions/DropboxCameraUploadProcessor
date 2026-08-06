e2ada6a0a15c22933108f0a78f8f687f04e127c0
Jeff Miles <twomilessolutions@gmail.com>
2026-08-06 13:01:06 -0500

    Convert to run off env file

---
+++ run_processor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#git pull
+
+#chmod +x run_processor.sh
+source .venv/bin/activate
+python3 CameraUploadsProcessor.py
+
+#./run.sh
---
+++ run_watchdog.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#git pull
+
+#chmod +x run_watchdog.sh
+source .venv/bin/activate
+python3 CameraUploadsWatchdog.py
+
+#./run.sh
